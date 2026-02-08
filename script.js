fcc1ec246e320e06c5cca0cae717632b43855695
suryakarthik139-star <suryakarthik139@gmail.com>
2026-02-08 08:40:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -60,7 +60,7 @@ noBtn.addEventListener("mouseover", () => {
 // YES is clicked
 
 yesBtn.addEventListener("click", () => {
-    title.textContent = "Yippeeee!";
+    title.textContent = "love you too!";
 
     catImg.src = "cat_dance.gif";
 
